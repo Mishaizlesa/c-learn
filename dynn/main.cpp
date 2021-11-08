6fdf4488085b47857079016a3c8dc5a2c3610c42
mixa <mihsa11ru@gmail.com>
2021-11-08 20:38:14 +0300

    123

--- dynn/main.cpp
+++ dynn/main.cpp
@@ -2,24 +2,17 @@
 #include <stdlib.h>
 int main() {
     char** w;
-    char** tmp;
     w=(char**)malloc(2*sizeof(char*));
-    tmp=(char**)malloc(2*sizeof(char*));
     for(int i=0;i<2;++i) w[i]=(char*)malloc(100*sizeof(char));
-    for(int i=0;i<2;++i) tmp[i]=(char*)malloc(100*sizeof(char));
     int k=2;
     int c=0;
     while(1){
         if (c==k){
             printf("переполнение\n");
+            char** tmp=(char**)malloc(k*2*sizeof(char*));for(int i=0;i<k*2;++i) tmp[i]=(char*)malloc(100*sizeof(char));
             for (int i=0;i<k;++i)for(int j=0;j<100;++j)tmp[i][j]=w[i][j];
             for(int i=0;i<k;++i) free(w[i]);
-            w=(char**)malloc(k*2*sizeof(char*));
-            for(int i=0;i<k*2;++i) w[i]=(char*)malloc(100*sizeof(char));
-            for (int i=0;i<k;++i)for(int j=0;j<100;++j)w[i][j]=tmp[i][j];
-            for(int i=0;i<k;++i) free(tmp[i]);
-            tmp=(char**)malloc(k*2*sizeof(char*));
-            for(int i=0;i<k*2;++i) tmp[i]=(char*)malloc(100*sizeof(char));
+            w=tmp;
             k=c*2;
         }
         scanf("%s",w[c]);
