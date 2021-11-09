c2a8036b4881bbc2d182374a11e990dab69b6dc3
mixa <mihsa11ru@gmail.com>
2021-11-09 14:13:58 +0300

    f

--- dynn/main.cpp
+++ dynn/main.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main() {
-    char** w;
+    char** w=(char**)malloc(2*sizeof(char*));for(int i=0;i<2;++i) w[i]=(char*)malloc(100*sizeof(char));
     int k=2;
     int c=0;
     while(1){
@@ -10,6 +10,7 @@ int main() {
             char** tmp=(char**)malloc(k*2*sizeof(char*));for(int i=0;i<k*2;++i) tmp[i]=(char*)malloc(100*sizeof(char));
             for (int i=0;i<k;++i)for(int j=0;j<100;++j)tmp[i][j]=w[i][j];
             for(int i=0;i<k;++i) free(w[i]);
+            free(w);
             w=tmp;
             k=c*2;
         }
