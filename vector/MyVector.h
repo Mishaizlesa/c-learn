6dac20aad52c531b0ddd8992cfeab2d31db26431
Mishaizlesa <62540571+Mishaizlesa@users.noreply.github.com>
2022-03-24 10:22:19 +0300

    Update MyVector.h

--- vector/MyVector.h
+++ vector/MyVector.h
@@ -1,6 +1,8 @@
 #ifndef MyVector_h
 #define MyVector_h
 #include <initializer_list>
+#include <iostream>
+#include <cmath>
 class Vector {
 private:
     int n = 0;
@@ -13,12 +15,14 @@ public:
     Vector& operator=(const Vector& v1);
     Vector operator +(const Vector& v1);
     Vector operator-(const Vector& v1);
-    friend std::ostream& operator<<(std::ostream& out,const Vector& v1);
     Vector& operator+=(const Vector& v1);
     Vector& operator-=(const Vector& v1);
     Vector operator*(const double c);
     Vector& operator*=(const double c);
+    friend std::ostream& operator<<(std::ostream& out,const Vector& v1);
+    friend std::istream& operator>>(std::istream& in,const Vector& v1);
     double  dot(const Vector& v1);
+    double norm();
     double& operator [](int i);
     int size();
     ~Vector();
