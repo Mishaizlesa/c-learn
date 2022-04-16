25ac257f71f7528937a38933e1fbe7660e54d0e5
Mishaizlesa <62540571+Mishaizlesa@users.noreply.github.com>
2022-04-16 14:25:05 +0300

    Update Matrix.h

--- Matrix/Matrix.h
+++ Matrix/Matrix.h
@@ -1,26 +1,36 @@
-#pragma once
-#include "Myvector.h"
+
+#ifndef Matrix_hpp
+#define Matrix_hpp
+
+#include <stdio.h>
+#include "MyVector.h"
 class Matrix {
 private:
-	int n = 0;
-	int m = 0;
-	Vector* data= NULL;
+    int n = 0;
+    int m = 0;
+    Vector* data= NULL;
 public:
-	Matrix(int n = 0, int m = 0);
-	Matrix(const Matrix& other);
-	Matrix& operator=(const Matrix& v1);
-	Matrix operator +(const Matrix& v1);
-	Matrix operator-(const Matrix& v1);
-	Matrix operator-();
-	Matrix& operator+=(const Matrix& v1);
-	Matrix& operator-=(const Matrix& v1);
-	Matrix operator*(const double c);
-	Matrix& operator*=(const double c);
-	Vector& operator [](int i);
-	Matrix operator *(const Matrix& v1);
-	friend std::ostream& operator<<(std::ostream& out, const Matrix& v1);
-	friend std::istream& operator>>(std::istream& in, const Matrix& v1);
-	double get_track();
-	int get_rank();
-	~Matrix();
+    Matrix(int n = 0, int m = 0);
+    Matrix(const Matrix& other);
+    Matrix& operator=(const Matrix& v1);
+    Matrix operator +(const Matrix& v1);
+    Matrix operator-(const Matrix& v1);
+    Matrix operator-();
+    Matrix& operator+=(const Matrix& v1);
+    Matrix& operator-=(const Matrix& v1);
+    Matrix operator*(const double c);
+    Matrix& operator*=(const double c);
+    Vector& operator [](int i);
+    Matrix operator *(Matrix v1);
+    friend std::ostream& operator<<(std::ostream& out, const Matrix& v1);
+    friend std::istream& operator>>(std::istream& in, const Matrix& v1);
+    Matrix T();
+    void gauss();
+    void swap_row(int i,int j);
+    bool  is_triangle();
+    long long determ();
+    double get_track();
+    int get_rank();
+    ~Matrix();
 };
+#endif /* Matrix_hpp */
